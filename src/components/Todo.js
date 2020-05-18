cefead4e4585a54775f9df350b53bfd1ce0d2607
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-18 13:50:52 +0530

    using multiple state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 const Todo = props => {
 
       const [todoName, setTodoName] = useState('');
+      const [todoList, setTodoList] = useState([]);
 
       const inputChangeHandler = event => {
           setTodoName(event.target.value);
+      };
+
+      const todoAddHandler = () => {
+           setTodoList(todoList.concat(todoName));
       };
 
      return (
@@ -15,8 +20,12 @@ const Todo = props => {
                placeholder="Todo" 
                onChange={inputChangeHandler} 
                value={todoName} />
-             <button type="button">ADD</button>
-             <ul />
+             <button type="button" onClick={todoAddHandler}>ADD</button>
+             <ul>
+                 {todoList.map(todo => (
+                     <li key={todo}>{todo}</li>
+                 ) )}
+             </ul>
          </React.Fragment>
      ) 
 };
